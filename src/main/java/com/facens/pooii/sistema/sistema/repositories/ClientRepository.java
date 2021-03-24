0966589b5673285d665324bc0d4003a74c6abdac
Glauco Todesco <glauco.todesco@gmail.com>
2021-03-23 22:29:33 -0300

    Add Page to Client  and Prod Profile

--- src/main/java/com/facens/pooii/sistema/sistema/repositories/ClientRepository.java
+++ src/main/java/com/facens/pooii/sistema/sistema/repositories/ClientRepository.java
@@ -2,10 +2,23 @@ package com.facens.pooii.sistema.sistema.repositories;
 
 import com.facens.pooii.sistema.sistema.entities.Client;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ClientRepository extends JpaRepository <Client,Long>{
+
+    //Consulta JPQL!!!!!
+    @Query("SELECT c FROM Client c " +
+           "WHERE " +
+           " ( LOWER(c.name)      LIKE   LOWER(CONCAT('%', :name,    '%')))  AND " +
+           " ( LOWER(c.address)   LIKE   LOWER(CONCAT('%', :address, '%')))      "
+    )
+
+    public Page <Client> find(Pageable pageRequest, String name, String address);
 
 }
